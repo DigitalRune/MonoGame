177967106fd4d83942d5464501965fe55a3ec347
DigitalRune <office@digitalrune.com>
2016-03-07 15:14:22 +0100

    NEW: Added GamePlatform.Null.cs

---
+++ MonoGame.Framework/GamePlatform.Null.cs
@@ -0,0 +1,14 @@
+// MonoGame - Copyright (C) The MonoGame Team
+// This file is subject to the terms and conditions defined in
+// file 'LICENSE.txt', which is part of this source code package.
+
+namespace Microsoft.Xna.Framework
+{
+    partial class GamePlatform
+    {
+        internal static GamePlatform PlatformCreate(Game game)
+        {
+            return null;
+        }
+    }
+}
