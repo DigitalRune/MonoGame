5f0d8dc5d24a4b21c56f5dc942196b7231dcca5c
DigitalRune <office@digitalrune.com>
2016-06-04 08:31:39 +0200

    Removed strong name signing.

--- MonoGame.Framework/Properties/AssemblyInfo.cs
+++ MonoGame.Framework/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
@@ -39,13 +39,12 @@ using System.Resources;
 
 // Allow the content pipeline assembly to access 
 // some of our internal helper methods that it needs.
-// [DIGITALRUNE] Needs to be public. Used in MonoGame.Framework.Content.Pipeline.
-//[assembly: InternalsVisibleTo("MonoGame.Framework.Content.Pipeline")]
-[assembly: InternalsVisibleTo("MonoGame.Framework.Net, PublicKey=0024000004800000940000000602000000240000525341310004000001000100b16c5e641ffa9b282535924aef3e9bb12285997689da5ede9c8e3e04f72b02f435119e0a9eb5344de9503fd2477c53fee7e9905ca2bdb78bb8c9000a936a92b0460ff5d798741d1a7a4d587fe119ad08e8011e6454d4c7a6a436055bb6cd7436277cfc13b8b6d9ebdf891d1bca1ed5609c58b9ab52c7c3893c297a77301c43c7")]
+[assembly: InternalsVisibleTo("MonoGame.Framework.Content.Pipeline")]
+[assembly: InternalsVisibleTo("MonoGame.Framework.Net")]
 
 //Tests projects need access too
-[assembly: InternalsVisibleTo("MonoGameTests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100b16c5e641ffa9b282535924aef3e9bb12285997689da5ede9c8e3e04f72b02f435119e0a9eb5344de9503fd2477c53fee7e9905ca2bdb78bb8c9000a936a92b0460ff5d798741d1a7a4d587fe119ad08e8011e6454d4c7a6a436055bb6cd7436277cfc13b8b6d9ebdf891d1bca1ed5609c58b9ab52c7c3893c297a77301c43c7")]
-[assembly: InternalsVisibleTo("MonoGame.Tests.Windows, PublicKey=0024000004800000940000000602000000240000525341310004000001000100b16c5e641ffa9b282535924aef3e9bb12285997689da5ede9c8e3e04f72b02f435119e0a9eb5344de9503fd2477c53fee7e9905ca2bdb78bb8c9000a936a92b0460ff5d798741d1a7a4d587fe119ad08e8011e6454d4c7a6a436055bb6cd7436277cfc13b8b6d9ebdf891d1bca1ed5609c58b9ab52c7c3893c297a77301c43c7")]
+[assembly: InternalsVisibleTo("MonoGameTests")]
+[assembly: InternalsVisibleTo("MonoGame.Tests.Windows")]
 
 #if !PORTABLE
 // Setting ComVisible to false makes the types in this assembly not visible 
